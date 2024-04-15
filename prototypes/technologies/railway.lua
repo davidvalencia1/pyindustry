22c4548288a0b66245fcfc816f401b7a239624fe
notnotmelon <zacharypicco@gmail.com>
2024-04-15 17:18:17 -0500

    https://github.com/pyanodon/pybugreports/issues/453

--- prototypes/technologies/railway.lua
+++ prototypes/technologies/railway.lua
@@ -1,59 +1,54 @@
-TECHNOLOGY {
-    type = "technology",
-    name = "railway-mk01",
-    icon = "__pyindustry__/graphics/technology/railway-mk01.png",
-    icon_size = 128,
-    order = "c-a",
-    prerequisites = {"steel-processing"},
-    effects = {},
-    unit = {
-        count = 150,
-        ingredients = {
-            {"automation-science-pack", 1},
-        },
-        time = 45
-    }
+data.raw.technology['railway'].icon = '__pyindustry__/graphics/technology/railway-mk01.png'
+data.raw.technology['railway'].icon_size = 128
+data.raw.technology['railway'].icon_mipmaps = nil
+data.raw.technology['railway'].prerequisites = {'steel-processing'}
+data.raw.technology['railway'].unit = {
+    count = 150,
+    ingredients = {
+        {'automation-science-pack', 1},
+    },
+    time = 45
 }
 
 if mods['pycoalprocessing'] then
-    TECHNOLOGY("railway-mk01"):add_prereq("wood-processing"):remove_prereq("steel-processing")
+    TECHNOLOGY('railway'):add_prereq('wood-processing'):remove_prereq('steel-processing')
 
     TECHNOLOGY {
-        type = "technology",
-        name = "railway-mk02",
-        icon = "__pyindustry__/graphics/technology/railway-mk02.png",
+        type = 'technology',
+        name = 'railway-mk02',
+        icon = '__pyindustry__/graphics/technology/railway-mk02.png',
         icon_size = 128,
-        order = "c-a",
-        prerequisites = {"railway-mk01", "niobium", "nexelit"},
-        dependencies = {"railway-mk01"},
+        order = 'c-a',
+        prerequisites = {'railway', 'niobium', 'nexelit'},
+        dependencies = {'railway'},
         effects = {},
         unit = {
             count = 250,
             ingredients = {
-                {"automation-science-pack", 1},
-                {"logistic-science-pack", 1},
+                {'automation-science-pack', 1},
+                {'logistic-science-pack', 1},
             },
             time = 45
         }
     }
 end
 
-if mods["pyhightech"] then
+if mods['pyhightech'] then
     TECHNOLOGY {
-        type = "technology",
-        name = "railway-mk03",
-        icon = "__pyindustry__/graphics/technology/railway-mk03.png",
+        type = 'technology',
+        name = 'railway-mk03',
+        icon = '__pyindustry__/graphics/technology/railway-mk03.png',
         icon_size = 128,
-        order = "c-a",
-        prerequisites = {"railway-mk02"},
-        dependencies = {"railway-mk02"},
+        order = 'c-a',
+        prerequisites = {'railway-mk02'},
+        dependencies = {'railway-mk02'},
         effects = {},
         unit = {
             count = 250,
             ingredients = {
-                {"automation-science-pack", 1},
-                {"logistic-science-pack", 1},
-                {"chemical-science-pack", 1}
+                {'automation-science-pack', 1},
+                {'logistic-science-pack', 1},
+                {'chemical-science-pack', 1}
             },
             time = 45
         }
@@ -63,22 +58,22 @@ end
 if mods['pyalternativeenergy'] then
 
     TECHNOLOGY {
-        type = "technology",
-        name = "railway-mk04",
-        icon = "__pyindustry__/graphics/technology/railway-mk04.png",
+        type = 'technology',
+        name = 'railway-mk04',
+        icon = '__pyindustry__/graphics/technology/railway-mk04.png',
         icon_size = 128,
-        order = "c-a",
-        prerequisites = {"railway-mk03","intermetallics-mk03","battery-mk04"},
-        dependencies = {"railway-mk03"},
+        order = 'c-a',
+        prerequisites = {'railway-mk03','intermetallics-mk03','battery-mk04'},
+        dependencies = {'railway-mk03'},
         effects = {},
         unit = {
             count = 400,
             ingredients = {
-                {"automation-science-pack", 1},
-                {"logistic-science-pack", 1},
-                {"military-science-pack", 1},
-                {"chemical-science-pack", 1},
-                {"production-science-pack", 1},
+                {'automation-science-pack', 1},
+                {'logistic-science-pack', 1},
+                {'military-science-pack', 1},
+                {'chemical-science-pack', 1},
+                {'production-science-pack', 1},
             },
             time = 45
         }
